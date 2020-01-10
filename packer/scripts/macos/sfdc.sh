cd28e0ca4a3c4d30cd3ed1bbf685c036d412fce7
vao <vao@salesforce.com>
2020-01-10 14:42:34 -0800

    gse test 24

--- packer/scripts/macos/sfdc.sh
+++ packer/scripts/macos/sfdc.sh
@@ -3,6 +3,8 @@
 /usr/bin/profiles install -path "/Users/anka/Desktop/profiles/CA Certificate.mobileconfig"
 /usr/bin/profiles install -path "/Users/anka/Desktop/profiles/enrollmentProfile.mobileconfig"
 
+osascript "/Users/anka/Desktop/profiles/user_accept.scpt"
+
 # /usr/bin/profiles install -path "/Users/anka/Desktop/profiles/CA Certificate.mobileconfig"
 # /usr/bin/profiles install -path "/Users/anka/Desktop/profiles/enrollmentProfile.mobileconfig"
 #
