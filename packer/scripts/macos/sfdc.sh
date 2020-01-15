2c310f3d869502ab2217ca9df2c6ac05d6a9415c
vao <vao@salesforce.com>
2020-01-15 14:31:45 -0800

    Quickpkg test 2

--- packer/scripts/macos/sfdc.sh
+++ packer/scripts/macos/sfdc.sh
@@ -3,7 +3,9 @@
 # /usr/bin/profiles install -path "/Users/anka/Desktop/profiles/CA Certificate.mobileconfig"
 # /usr/bin/profiles install -path "/Users/anka/Desktop/profiles/enrollmentProfile.mobileconfig"
 
-/usr/sbin/installer -pkg "/Users/anka/Desktop/profiles/QuickAddDev-10.21.2018.pkg" -target /
+# /usr/sbin/installer -pkg "/Users/anka/Desktop/profiles/QuickAddDev-10.21.2018.pkg" -target /
+/usr/sbin/installer -pkg "/Users/anka/Desktop/profiles/Production_QuickAdd_5.19.pkg" -target /
+
 
 ADCompName="gse-vm"
 
