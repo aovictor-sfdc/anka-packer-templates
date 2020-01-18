c4a2d6b7216ea0c2fa692a8b42268587a9413246
vao <vao@salesforce.com>
2020-01-18 15:19:21 -0800

    prov test 5

--- packer/scripts/macos/gse-quickadd.sh
+++ packer/scripts/macos/gse-quickadd.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# /usr/sbin/installer -pkg "/Users/anka/Desktop/profiles/QuickAddDev-10.21.2018.pkg" -target /
-/usr/sbin/installer -pkg "/Users/anka/Desktop/profiles/Production_QuickAdd_5.19.pkg" -target /
-
-
+# set comp name
 ADCompName="gse-qa-vm"
 
-/usr/sbin/scutil --set ComputerName "$ADCompName"
-/usr/sbin/scutil --set LocalHostName "$ADCompName"
-/usr/sbin/scutil --set HostName "$ADCompName.internal.salesforce.com"
+/usr/sbin/scutil --set ComputerName "$ADCompName" && echo "ComputerName set"
+/usr/sbin/scutil --set LocalHostName "$ADCompName" && echo "LocalHostName set"
+/usr/sbin/scutil --set HostName "$ADCompName.internal.salesforce.com" && echo "Hostname set"
+
+# Enroll into jamf
+/usr/sbin/installer -pkg "/Users/anka/Desktop/profiles/Production_QuickAdd_5.19.pkg" -target / &
+echo "jamf enrolled."
+
+# kill any jamf prompt
+echo "sleep 90 while enrolling"
+sleep 90
+/usr/bin/killall KeyboardSetupAssistant && echo "keyboard setup ast killed"
+/usr/bin/killall jamfHelper && echo "jamfHelper killed"
+echo "sleep 30 for policies"
+sleep 30
+
+exit 0
