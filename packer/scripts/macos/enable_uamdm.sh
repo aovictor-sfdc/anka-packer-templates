3f2c4b4f34c9b04548c4b13c781ce12f3a4dd7ac
vao <vao@salesforce.com>
2020-01-19 00:10:05 -0800

    prov test 9

--- packer/scripts/macos/enable_uamdm.sh
+++ packer/scripts/macos/enable_uamdm.sh
@@ -5,36 +5,52 @@ vm_name=$(anka list | grep -i "$identifier" | awk -F\| '{print $2}' | tr -d ' ')
 
 if ! [[ $(anka show "$vm_name" status) == running ]]; then
   echo "vm not running."
-  if anka show "$vm_name" status | grep -E '(suspended|stopped)'; then
-    anka start "$vm_name" && echo "vm started"
+  if anka show "$vm_name" status | grep -E suspended; then
+    anka start "$vm_name" && echo "vm started from suspension"
+    echo "waiting a 10 seconds for machine to boot..."
+    sleep 10
+  elif anka show "$vm_name" status | grep -E stopped; then
+    anka start "$vm_name" && echo "vm started from stop"
+    echo "waiting a 30 seconds for machine to boot..."
+    sleep 30
   fi
 else
   echo "vm is running already"
 fi
-
-
-echo "waiting a 30 seconds for machine to boot..."
-sleep 30
 
 VNCSTR=$(anka show "$vm_name" vnc_connection_string | awk -F\/\/ '{print $NF}')
 
 echo "VNCSTR: $VNCSTR"
 
 if which /Users/vao/.pyenv/shims/vncdo; then
+  # enable UAMDM profiles
+  echo "enable UAMDM profiles...15 seconds..."
   /Users/vao/.pyenv/shims/vncdo -s "${VNCSTR}" -p admin move 220 180 click 1 pause 2 \
     move 0 0 click 1 pause 2 \
     type "s" key enter pause 2 \
     type "profiles" pause 2 \
     key return pause 2 \
-    key tab pause 2\
-    key down pause 2\
     move 570 240 click 1 pause 2 \
     move 630 240 click 1
+  # enable filevault
+  echo "enabling filevault...60 seconds...."
+  /Users/vao/.pyenv/shims/vncdo -s "${VNCSTR}" -p admin move 0 0 click 1 pause 2 \
+    type "l" \
+    key down \
+    key return pause 2 \
+    key return pause 5 \
+    type "admin" \
+    key return pause 5 \
+    key return pause 30 \
+    key return pause 5 \
+    type "a" \
+    key return pause 2 \
+    type "admin" pause 2 \
+    key return
 else
   echo "vncdo not installed"
   exit 1
 fi
 
-sleep 120
 anka suspend "$vm_name" && echo "$vm suspended."
 exit 0
--- packer/scripts/macos/gse-quickadd.sh
+++ packer/scripts/macos/gse-quickadd.sh
@@ -3,20 +3,20 @@
 # set comp name
 ADCompName="gse-qa-vm"
 
-/usr/sbin/scutil --set ComputerName "$ADCompName" && echo "ComputerName set"
-/usr/sbin/scutil --set LocalHostName "$ADCompName" && echo "LocalHostName set"
-/usr/sbin/scutil --set HostName "$ADCompName.internal.salesforce.com" && echo "Hostname set"
+/usr/sbin/scutil --set ComputerName "$ADCompName" && echo "ComputerName set $ADCompName."
+/usr/sbin/scutil --set LocalHostName "$ADCompName" && echo "LocalHostName set $ADCompName."
+/usr/sbin/scutil --set HostName "$ADCompName.internal.salesforce.com" && echo "Hostname set $ADCompName.internal.salesforce.com."
 
 # Enroll into jamf
 /usr/sbin/installer -pkg "/Users/anka/Desktop/profiles/Production_QuickAdd_5.19.pkg" -target / &
 echo "jamf enrolled."
 
 # kill any jamf prompt
-echo "sleep 90 while enrolling"
-sleep 90
+echo "sleep 60 while enrolling"
+sleep 60
 /usr/bin/killall KeyboardSetupAssistant && echo "keyboard setup ast killed"
 /usr/bin/killall jamfHelper && echo "jamfHelper killed"
-echo "sleep 30 for policies"
-sleep 30
+echo "sleep 60 for policies"
+sleep 60
 
 exit 0
